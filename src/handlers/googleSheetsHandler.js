9e4f5b8037f25dc56567d74a7caf12768f8c4499
caina-barbosa <caina.barbosa@trilogy.com>
2023-10-16 17:09:01 -0300

    Handler now updates faulty row

--- src/handlers/googleSheetsHandler.js
+++ src/handlers/googleSheetsHandler.js
@@ -20,6 +20,33 @@ const auth = new google.auth.JWT(
 );
 
 /**
+ * Updates the Google Sheet with new CourseName for a given row.
+ * 
+ * @param {number} rowIndex 
+ * @param {string} newCourseName 
+ */
+const updateCourseNameInSheet = async (rowIndex, newCourseName) => {
+    try {
+        console.log(newCourseName, rowIndex)
+        await sheets.spreadsheets.values.update({
+            auth,
+            spreadsheetId: SPREADSHEET_ID,
+            range: `${SHEET_NAME}!G${rowIndex + 2}`, // Column G is "Course Name", adjusting for 0-based array
+            valueInputOption: 'RAW',
+            resource: {
+                values: [[newCourseName]]
+            }
+        });
+    } catch (error) {
+        console.error('Error updating Google Sheets:', error);
+        throw error;
+    }
+};
+
+
+
+
+/**
  * Finds a row in the Google Sheet based on the provided parameters.
  * 
  * @param {string} Name 
@@ -47,15 +74,21 @@ export const findRowInSheet = async (Name, Subject, CourseName, AppName) => {
         }
 
         // Check for matching row.
-        for (let i = 0; i < rows.length; i++) { 
+        for (let i = 1; i < rows.length; i++) {
             const row = rows[i];
-            if (
-                row[1] === Name &&          // "Name" column
-                row[5] === Subject &&       // "Subject" column
-                row[6] === CourseName &&    // "Course Name" column
-                row[7] === AppName       
-            ) {
-                // Convert row array to a well-formatted JSON object.
+            const isPerfectMatch = (
+                row[1] === Name &&
+                row[5] === Subject &&
+                row[6] === CourseName &&
+                row[7] === AppName
+            );
+            const isPartialMatch = (
+                row[1] === Name &&
+                row[5] === Subject &&
+                row[7] === AppName
+            );
+    
+            if (isPerfectMatch) {
                 const formattedRow = {
                     'Student & Campus ID': row[0],
                     'Name': row[1],
@@ -65,11 +98,28 @@ export const findRowInSheet = async (Name, Subject, CourseName, AppName) => {
                     'Course Name': row[6],
                     'App Name': row[7],
                     'Start': row[8],
+                    'IsItCorrectlyRostered': 1
+                };
+                return formattedRow;
+            } else if (isPartialMatch) {
+                await updateCourseNameInSheet(i, CourseName);
+                row[6] = CourseName;  
+    
+                const formattedRow = {
+                    'Student & Campus ID': row[0],
+                    'Name': row[1],
+                    'LVL': row[2],
+                    'Username': row[3],
+                    'Subject': row[5],
+                    'Course Name': row[6],
+                    'App Name': row[7],
+                    'Start': row[8],
+                    'IsItCorrectlyRostered': 0
                 };
                 return formattedRow;
             }
         }
-
+    
         return null;
     } catch (error) {
         console.error('Error accessing Google Sheets:', error);
